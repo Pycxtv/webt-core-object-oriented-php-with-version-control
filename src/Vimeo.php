dca33a1e9d2335dff3d428047791e06684153613
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2023-09-30 19:48:17 +0200

    User Story 8: Vimeo Class

---
+++ src/Vimeo.php
@@ -0,0 +1,25 @@
+<?php 
+
+require_once('Video.php');
+
+class Vimeo extends Video {
+
+    private $id;
+
+    public function __construct($name, $id)
+    {
+        parent::__construct($name);
+        $this->id = $id;
+    }
+
+
+    function getSource()
+    {
+        return "Vimeo";
+    }
+
+    function getHTML()
+    {
+        return "<iframe src=\"https://player.vimeo.com/video/{$this->id}\" width=\"640\" height=\"268\" frameborder=\"0\" allow=\"autoplay; fullscreen; picture-in-picture\" allowfullscreen></iframe>";
+    }
+}
