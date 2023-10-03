05dffc400a9ea9ec2781b57bcece658e4315dd8c
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2023-10-03 08:47:38 +0200

    added Vimeo videos

--- index.php
+++ index.php
@@ -7,7 +7,11 @@ use Onphpoint\ObjectOrientedPhp\Vimeo;
 
 $videos = [];
 
-$videos[] = new Vimeo("name", "id");
+$videos[] = new Vimeo("FOOD", "305185528");
+$videos[] = new Vimeo("Food Glorious Food Reel - Taller Stories", "415181652");
+$videos[] = new Vimeo("Food demoreel", "303516290");
+$videos[] = new Vimeo("Food Trip | Food Showreel 2020", "445903081");
+$videos[] = new Vimeo("Christmas On FOOD - LifeStyle FOOD Channel", "38407821");
 $videos[] = new YouTube("name", "id");
 
 
