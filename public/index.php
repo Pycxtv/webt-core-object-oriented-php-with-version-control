c273b2557e32b5406b8aacea97adb4793587d206
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2023-10-24 08:25:45 +0200

    added sass with compiler

--- public/index.php
+++ public/index.php
@@ -21,14 +21,14 @@ $videos[] = new Vimeo("Christmas On FOOD - LifeStyle FOOD Channel", "38407821");
 
 echo <<<'HTML'
 <!DOCTYPE html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Foodtube</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Foodtube</h1>
+<p class="h1">Foodtube</p>
 <main>
 HTML;
 
